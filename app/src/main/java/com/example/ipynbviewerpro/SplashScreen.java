893bebe4b1eeb1d84304909dad1dab9320b6341f
Solomon <solomon258@gmail.com>
2023-11-16 15:12:08 +0530

    Completed :intent-filters(partial) Pending : save as pdf, optional-streamlis site

---
+++ app/src/main/java/com/example/ipynbviewerpro/SplashScreen.java
@@ -0,0 +1,64 @@
+package com.example.ipynbviewerpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.UriPermission;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import java.util.List;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 3000;
+    private static final int SPLASH_DISPLAY_LENGTH = 2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        if (getIntent().getAction() != null && getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            Uri fileUri = getIntent().getData();
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                if (fileUri != null) {
+                    if(hasPersistableUriPermission(fileUri)){
+                        getContentResolver().takePersistableUriPermission(
+                                fileUri,
+                                Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                        );
+                    }
+                    // Start WebActivity and pass the file URI
+                    Intent webIntent = new Intent(this, Webview.class);
+                    webIntent.putExtra("filePath", fileUri.toString());
+                    startActivity(webIntent);
+                    finish(); // Close the SplashActivity
+                }
+            }, SPLASH_DISPLAY_LENGTH);
+        }else{
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(getApplicationContext(), HomePage.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, SPLASH_SCREEN_TIME_OUT);
+        }
+    }
+
+    private boolean hasPersistableUriPermission(Uri uri) {
+        List<UriPermission> uriPermissions = getContentResolver().getPersistedUriPermissions();
+        for (UriPermission permission : uriPermissions) {
+            if (permission.getUri().equals(uri)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
